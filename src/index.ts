9dd55b738a8f2216af60cd902a15958b746f6d2b
Anmol <anmolp476@gmail.com>
2026-05-15 15:51:35 -0400

    Wrapped the express server using a basic HTTP server. Then created a web socket server using that. Also had the '/' path send an HTML script string, that creates a new web socket for the client, then connects the client to the web socket server.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express, { type Request, type Response } from 'express'
+import { createServer } from 'http'
+import { WebSocketServer, WebSocket } from 'ws'
 
-const app = express()
-const port = 3000
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) =>  {
-    res.send("Hello World!")
+    res.send("<script> const socket = new WebSocket('ws://10.0.0.212:3000'); socket.onopen = () => console.log('Connected!'); </script>");
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+const server = createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+    console.log("New client connected!");
+
+    ws.on('message', (message: string) => {
+        console.log(`Recieved: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Express and WebSocket server running on http://localhost:${port}`)
 })
